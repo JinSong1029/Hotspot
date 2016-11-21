7478d4a2cd065ab0f436533b75388cb912321caf
TigerDevCom <antonio.lee327@gmail.com>
2016-11-21 14:42:37 -0800

    migration created

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161121212331) do
+ActiveRecord::Schema.define(version: 20161121215521) do
+
+  create_table "auto_teller_machines", force: :cascade do |t|
+    t.string   "type"
+    t.integer  "code"
+    t.text     "u_id"
+    t.text     "brand"
+    t.text     "address"
+    t.text     "city"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
